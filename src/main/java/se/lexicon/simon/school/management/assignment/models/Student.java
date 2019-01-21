174141c3266f0655213f47a71682efd34db43738
deltagare <deltagare@192.168.10.100>
2019-01-21 14:37:36 +0100

    Added a constructor

--- src/main/java/se/lexicon/simon/school/management/assignment/models/Student.java
+++ src/main/java/se/lexicon/simon/school/management/assignment/models/Student.java
@@ -1,22 +1,27 @@
 package se.lexicon.simon.school.management.assignment.models;
 
+/**
+ * 
+ * @author Mattias Hansson
+ *
+ */
 public class Student {
-
 	
-	/**
-	 * private int id
-	 * private String name
-	 * private string email
-	 * private String addres
-	 * ( get / set	
-	 */
 		
 		 private int id;
 		 private String name;
 		 private String email;
 		 private String addres;
-		 //get / set	
-		public int getId() {
+		 
+		 Student(int id, String name, String email, String addres){
+			 this.id = id;
+			 this.name = name;
+			 this.email = email;
+			 this.addres = addres;
+		 }
+
+		 
+		 public int getId() {
 			return id;
 		}
 		public void setId(int id) {
