f641742e5ac670e6156e3887fbb28ffe4379352a
deltagare <deltagare@192.168.10.100>
2019-01-21 12:14:30 +0100

    Created student class

--- src/main/java/se/lexicon/simon/school/management/assignment/models/Student.java
+++ src/main/java/se/lexicon/simon/school/management/assignment/models/Student.java
@@ -2,8 +2,47 @@ package se.lexicon.simon.school.management.assignment.models;
 
 public class Student {
 
-	public Student() {
-		// TODO Auto-generated constructor stub
-	}
+	
+	/**
+	 * private int id
+	 * private String name
+	 * private string email
+	 * private String addres
+	 * ( get / set	
+	 */
+		
+		 private int id;
+		 private String name;
+		 private String email;
+		 private String addres;
+		 //get / set	
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getAddres() {
+			return addres;
+		}
+		public void setAddres(String addres) {
+			this.addres = addres;
+		}
+		 
+		
+		
+	
 
 }
